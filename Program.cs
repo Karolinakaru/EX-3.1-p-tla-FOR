77d64dc2ab0a44a663974c1356f58843e74c87ab
singhet <singhet@SCSWL4072>
2024-01-18 19:28:28 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Value of y = root(3^2)*x is:");
+        for (int x = 0; x<=10; x++)
+        {
+            double y = Math.Sqrt(Math.Pow(3, 2)) * x;
+            
+            Console.WriteLine($"{y}, x= {x}");
+        }
+        
+    }
+}
